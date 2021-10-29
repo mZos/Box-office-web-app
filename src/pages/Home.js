ef4a15bc5510c516c40cc20475fcd87764674eba
mZos <mohdzakariya272@gmail.com>
2021-10-29 19:08:22 +0530

    created preview card for api data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import ActorGrid from '../component/actor/ActorGrid';
 import MainPageLayout from '../component/MainPageLayout';
+import ShowGrid from '../component/show/ShowGrid';
 import { apiGetResults } from '../misc/config';
 
 const Home = () => {
@@ -32,13 +34,12 @@ const Home = () => {
     }
 
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
-
     return null;
   };
 
