112eff21b5433cfefddf0db42bd92fba75ce0e72
mZos <mohdzakariya272@gmail.com>
2021-11-01 23:05:38 +0530

    decreased the size of cards by ~50px

--- src/component/Styled.js
+++ src/component/Styled.js
@@ -8,14 +8,14 @@ export const FlexGrid = styled(FadeIn)`
 `;
 
 export const SearchCard = styled.div`
-  width: 300px;
+  width: 260px;
   height: 100%;
   margin: 0 15px 40px;
 
   .img-wrapper {
     width: 100%;
     border-radius: 40px;
-    height: 420px;
+    height: 370px;
     overflow: hidden;
     border: 1px solid #ddd;
 
